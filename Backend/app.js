5fe342dcf4d10d8e3186fc64506b26436578c2c0
Kavish Jain <kavishjain307@gmail.com>
2026-08-18 13:32:41 +0530

    Add complete frontend and backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 3000
+const dotenv = require("dotenv");
+const PORT = process.env.PORT
+
+dotenv.config();
 
 app.use(express.json());
 
